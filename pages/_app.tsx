aa7cb22be64baf635479999b142cd12a2c028b45
Saransh Sharma <saranshis@pm.me>
2023-08-13 11:24:58 +0100

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import store from "@redux/store";
+import "@styles/global.css";
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
