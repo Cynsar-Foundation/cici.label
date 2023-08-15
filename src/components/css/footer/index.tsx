b9c4fcb88be2567f68d6eeed047e1b5d9b5926b0
Saransh Sharma <saranshis@pm.me>
2023-08-15 10:18:58 +0100

    Header and Footer Sorted

--- src/components/css/footer/index.tsx
+++ src/components/css/footer/index.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import React from "react";
+import { useSelector } from "react-redux";
 
 import styles from "./index.module.css";
 
 export const Footer: React.FC = () => {
+  const footer = useSelector((state: any) => state.directUs.title)
   return (
-    <div className="deco deco--title">2023 Jaal Yantra and Cici Label Produce</div>
+    <div className="deco deco--title">{footer.footer}</div>
   );
 };
